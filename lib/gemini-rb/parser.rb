190017d84f10f53c47fa10a595aa58ef3accc68b
Pjotr Prins <pjotr.public01@thebird.nl>
2021-08-01 10:20:36 +0200

    GMI block parser

--- lib/gemini-rb/parser.rb
+++ lib/gemini-rb/parser.rb
@@ -1,8 +1,80 @@
-# Main parser for the gemini format
+# Main parser for the gemini format. The parser splits the input buffer into lines
+# and returns a list of items with keywords. The basics are:
+#
+# [
+#  { type: :header, level: 1, content: "Gemtext cheatsheet" },
+#  { type: :text, content: [ "Here's the basics of how text works in Gemtext:"] },
+#  { type: :list, content: "item" },
+#  { type: :quote, content: "item" },
+#  { type: :verbatim, content: [ "line1", "line2"] },
+#  { type: :uri, link: "gemini://xxx.yyy", content: "This is a link" }
+# ]
+#
+# New lines are embedded in :text and :verbatim as "". Between types they are assumed.
+#
+# See also the included test module
 
 module Gemini
 
-  def parse(buf)
+  # Very simple block splitter. Makes sure text and verbatim blocks are together and
+  # other lines are typed. That is all it should do! Resulting in
+  # [{:type=>:header, :content=>"# Gemtext cheatsheet"},
+  #  {:type=>:text, :content=>["Here's the basics of how text works in Gemtext:"]},
+  #   {:type=>:list, :content=>"* Long lines get wrapped by the client to fit the screen"},
+  #   {:type=>:list, :content=>"* Short lines *don't* get joined together"},
+  #   {:type=>:list, :content=>"* Write paragraphs as single long lines"},
+  #   {:type=>:list, :content=>"* Blank lines are rendered verbatim"},
+  # {:type=>:header, :content=>"## Headings"},
+  #  {:type=>:text, :content=>["You get three levels of heading:"]},
+  #  {:type=>:verbatim, :content=>["# Heading", "", "## Sub-heading", "", "### Sub-subheading"]}, ... ]
+  #
+  # Futher transformations should happen in other methods
+  def parse_blocks(buf)
+    lines = buf.split("\n")
+    list = []
+    h = {}
+    inblock = false
+    lines.each do |line|
+      l = line.strip
+      # First decide if we have a new type
+      if inblock
+        if h[:type] == :verbatim
+          if l =~ /^```/
+            inblock = false
+          else
+            h[:content].push l
+          end
+        else # type == :text
+          if l == ""
+            inblock = false
+          else
+            h[:content].push l
+          end
+        end
+      else
+        if l == ""
+          next
+        elsif l =~ /^#/
+          h = { type: :header, content: l }
+        elsif l =~ /^\*/
+          h = { type: :list, content: l }
+        elsif l =~ /^\>/
+          h = { type: :quote, content: l }
+        elsif l =~ /^\=>/
+          h = { type: :uri, content: l }
+        elsif l =~ /^```/
+          h = { type: :verbatim, content: [] }
+          inblock = true
+        else
+          h = { type: :text, content: [ l ] }
+          inblock = true
+        end
+      end
+      if !inblock
+        list.push(h)
+      end
+    end
+    list
   end
 
 end
--- test/parser-test.rb
+++ test/parser-test.rb
@@ -1,15 +1,17 @@
 
 require_relative '../lib/gemini-rb/parser'
 require 'minitest/autorun'
+require 'pp'
 
 
 class ParserTest < MiniTest::Test
 
   include Gemini
 
-  def test_normal
-    gmi = parse(File.read("data/test.gmi")
-    # assert_equal 24, factorial(4),"4! should be 24"
+  def test_parse_blocks
+    gmi = parse_blocks(File.read("data/test01.gmi", encoding: "UTF-8"))
+    pp gmi
+    assert_equal {:type=>:header, :content=>"# Gemtext cheatsheet"}, gmi[0]
   end
 
 end
